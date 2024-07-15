f37af41b57ad2f514bc27a1691d7a09eff100834
Giuseppe Magliano <91559694+root-over@users.noreply.github.com>
2024-07-15 16:25:16 +0200

    Euristica finale

--- classical/src/planning/PlanningHeuristic.java
+++ classical/src/planning/PlanningHeuristic.java
@@ -7,29 +7,46 @@ import fr.uga.pddl4j.problem.State;
 import fr.uga.pddl4j.problem.operator.Condition;
 import fr.uga.pddl4j.util.BitVector;
 
-public class PlanningHeuristic extends RelaxedGraphHeuristic {
+/**
+ * HeuristicEvaluation estende la RelaxedGraphHeuristic per fornire
+ * una valutazione euristica specializzata per problemi di pianificazione.
+ */
+public class HeuristicEvaluation extends RelaxedGraphHeuristic {
 
-    private BitVector ist;
+    private BitVector goalFluents;
 
-    public PlanningHeuristic(Problem problem) {
+    /**
+     * Il costruttore inizializza l'euristica con i goal fluents del problema.
+     * @param problem Il problema di pianificazione.
+     */
+    public HeuristicEvaluation(Problem problem) {
         super(problem);
-        super.setAdmissible(false);
-        this.ist = problem.getGoal().getPositiveFluents();
+        this.setAdmissible(false);
+        this.goalFluents = problem.getGoal().getPositiveFluents();
     }
 
+    /**
+     * Stima il costo per raggiungere l'obiettivo dallo stato attuale.
+     * @param state Lo stato attuale.
+     * @param goal La condizione di obiettivo.
+     * @return Il costo stimato per raggiungere l'obiettivo.
+     */
     public int estimate(State state, Condition goal) {
-        if(state.satisfy(goal))
-            return 0;
-        super.setGoal(goal);
-        super.expandRelaxedPlanningGraph(state);
-        return super.isGoalReachable() ? super.getRelaxedPlanValue() + ist.cardinality() - state.getIntersection(ist).cardinality() : Integer.MAX_VALUE;
-
+        if (state.satisfy(goal)) return 0;
+        this.setGoal(goal);
+        this.expandRelaxedPlanningGraph(state);
+        int goalDistance = this.isGoalReachable() ? this.getRelaxedPlanValue() : Integer.MAX_VALUE;
+        int heuristicValue = goalDistance + goalFluents.cardinality() - state.getIntersection(goalFluents).cardinality();
+        return heuristicValue;
     }
 
-
+    /**
+     * Sovraccarica il metodo estimate per funzionare direttamente con un Nodo.
+     * @param node Il nodo che rappresenta uno stato nello spazio di ricerca.
+     * @param goal La condizione di obiettivo.
+     * @return Il costo stimato per raggiungere l'obiettivo dallo stato del nodo.
+     */
     public double estimate(Node node, Condition goal) {
-        return (double)this.estimate((State)node, goal);
+        return (double) this.estimate((State) node, goal);
     }
-
 }
-
